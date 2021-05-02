6c4ea6e178baf1f7f8b0770f4cd39ed7e5609f05
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-05-02 18:04:37 +0530

    updated may 2nd

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,15 +6,27 @@ const Header = () => {
   return (
     <HeaderImg>
       <Image src={logo} alt='vikings' />
+      <Headerh5>
+        Scores are not updated live. They're typically updated once daily.
+        Scores were last updated at roughly 10:00 AM IST on May 2nd.
+      </Headerh5>
     </HeaderImg>
   )
 }
 
 const HeaderImg = styled.header`
   height: 300px;
-  display: flex;
   align-items: center;
   justify-content: center;
+  @media (min-width: 480px) and (max-width: 768px) {
+    height: 200px;
+  }
+  @media (max-width: 479px) {
+    height: 100px;
+  }
+`;
+
+const Headerh5 = styled.h5`
   @media (min-width: 480px) and (max-width: 768px) {
     height: 200px;
   }
