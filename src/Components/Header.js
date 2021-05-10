7182e30ab8a0eb61ae0883f6246dfd16e12f23d3
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-05-10 14:30:30 +0530

    may 10th

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <Image src={logo} alt='vikings' />
       <Headerh5>
         Scores are not updated live. They're typically updated once daily.
-        Scores were last updated at roughly 10:00 AM IST on May 9.
+        Scores were last updated at roughly 10:00 AM IST on May 10th.
       </Headerh5>
     </HeaderImg>
   )
